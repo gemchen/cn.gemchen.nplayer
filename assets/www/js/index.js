ec7709bda91dffc0f510db924fbce804d3d906d2
gemchen <aubama@gmail.com>
2013-07-18 21:45:53 +0800

    init
    add new

---
+++ assets/www/js/index.js
@@ -0,0 +1,10 @@
+function showVideoList () {
+    $("#videoListDiv")[0].style.display = "block";
+    $("#downloadListDiv")[0].style.display = "none";
+}
+
+
+function showDownloadList () {
+    $("#videoListDiv")[0].style.display = "none";
+    $("#downloadListDiv")[0].style.display = "block";
+}
