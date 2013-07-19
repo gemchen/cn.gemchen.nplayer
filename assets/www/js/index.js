e105edc4e608aea96f98b8512945e4dc6c391d93
gemchen <aubama@gmail.com>
2013-07-19 23:43:12 +0800

    [update]add manage page

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,10 +1,63 @@
+/**************
+* View
+**************/
 function showVideoList () {
-    $("#videoListDiv")[0].style.display = "block";
-    $("#downloadListDiv")[0].style.display = "none";
+    $("div.videoList").toggleClass("hide",false);
+    $("div.downloadList").toggleClass("hide",true);
+    $("div.manageList").toggleClass("hide",true);
 }
 
 
 function showDownloadList () {
-    $("#videoListDiv")[0].style.display = "none";
-    $("#downloadListDiv")[0].style.display = "block";
+    $("div.videoList").toggleClass("hide",true);
+    $("div.downloadList").toggleClass("hide",false);
+    $("div.manageList").toggleClass("hide",true);
+
+    loadDownloadFiles();
+}
+
+function showManageDiv () {
+    $("div.videoList").toggleClass("hide",true);
+    $("div.downloadList").toggleClass("hide",true);
+    $("div.manageList").toggleClass("hide",false);
+}
+
+function delSubmit (isDel) {
+	$('input[type="checkbox"]').each(function () {
+		if($(this).is(':checked')){
+			if(isDel){
+				alert($(this).attr("id"));
+			}else{
+				$(this).attr("checked",false).checkboxradio("refresh");
+				showDownloadList();
+			}
+		}
+	});
+}
+
+/****************
+* Logical
+****************/
+function fail (evt) {
+	console.log(evt.target.error.code);
+}
+
+function loadDownloadFiles () {
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, getFS, fail);
+}
+
+function getFS (fileSystem) {
+	fileSystem.root.getDirectory("nplayer", null, getDirectoryEntry, fail);
+}
+
+function getDirectoryEntry (dirEntry) {
+	var dirReader = dirEntry.createReader();
+	dirReader.readEntries(readDirEntriesSuccess,fail);
+}
+
+function readDirEntriesSuccess (entries) {
+	var i;
+	for (i=0; i < entries.length; i++) {
+		console.log("###"+entries[i].name);
+	};
 }
